10d0ca17467eaf22e5616b9608c825d86abbee35
Mohamed Amine Hamdouni <mohamed@mvst.co>
2022-05-28 10:06:28 +0100

    finish submenu

--- frontend/components/Menu.tsx
+++ frontend/components/Menu.tsx
@@ -1,52 +1,33 @@
 import clsx from "clsx";
-import { useState } from "react";
+import { MenuProps } from "../pages";
 import { PictureIcon } from "../public/icons/Picture.icon";
 import { UrlIcon } from "../public/icons/Url.icon";
 import { UserIcon } from "../public/icons/User.icon";
 
-export default function Menu() {
-	const [active, setActive] = useState([false, false, false]);
-	const clickHandler = (i: number) => {
-		setActive(
-			active.map((value, index) => {
-				if (index === i) return true;
-				else return false;
-			})
-		);
-		console.log(active);
-	};
+export default function Menu(menu: { menu: MenuProps; clickHandler: (i: number) => void; active: boolean[] }) {
 	return (
-		<div className="h-screen p-5 box-sizing">
-			<div className="w-[300px] py-10  bg-neutral-900 rounded-3xl text-white font-sans flex flex-col self-stretch drop-shadow-lg">
+		<div className="h-fit p-5 box-sizing  w-[350px]">
+			<div className="w-full py-10  bg-neutral-900 rounded-3xl text-white font-sans flex flex-col self-stretch drop-shadow-lg">
 				<div className="flex w-full justify-center mb-10">
 					<h1 className="text-2xl font-semibold tracking-widest"> instify </h1>
 				</div>
 				<div
-					className={clsx("flex w-full justify-left gap-3 items-center py-5 px-10 cursor-pointer hover:bg-neutral-800 transition-all duration-300", { "border-l border-l-8 border-white": active[0] })}
+					className={clsx("flex w-full justify-left gap-3 items-center py-5 px-10 cursor-pointer hover:bg-neutral-800 transition-all duration-300", { "border-l border-l-8 border-white": menu.active[0] })}
 					onClick={() => {
-						clickHandler(0);
+						menu.clickHandler(0);
 					}}
 				>
 					<UserIcon className="w-4 h-4 stroke-white" />
 					<h1 className="text-base font-normal tracking-widest">Manage Account</h1>
 				</div>
 				<div
-					className={clsx("flex w-full justify-left gap-3 items-center py-5 px-10 cursor-pointer hover:bg-neutral-800 transition-all duration-300", { "border-l border-l-8 border-white": active[1] })}
+					className={clsx("flex w-full justify-left gap-3 items-center py-5 px-10 cursor-pointer hover:bg-neutral-800 transition-all duration-300", { "border-l border-l-8 border-white": menu.active[1] })}
 					onClick={() => {
-						clickHandler(1);
+						menu.clickHandler(1);
 					}}
 				>
 					<PictureIcon className="w-4 h-4 stroke-white" />
 					<h1 className="text-base font-normal tracking-widest">Manage Pictures</h1>
-				</div>
-				<div
-					className={clsx("flex w-full justify-left gap-3 items-center py-5 px-10 cursor-pointer hover:bg-neutral-800 transition-all duration-300", { "border-l border-l-8 border-white": active[2] })}
-					onClick={() => {
-						clickHandler(2);
-					}}
-				>
-					<UrlIcon className="w-4 h-4 stroke-white" />
-					<h1 className="text-base font-normal tracking-widest">Validate Path</h1>
 				</div>
 				<div className="w-full mt-20 flex justify-center text-center flex-col">
 					<p className="text-sm">
--- frontend/pages/index.tsx
+++ frontend/pages/index.tsx
@@ -1,12 +1,52 @@
 import { NextPage } from "next";
+import { useState } from "react";
 import Menu from "../components/Menu";
+import { Submenu } from "../components/Submenu";
+export interface SubmenuProps {
+	elements: { title: string }[];
+}
+export interface MenuProps {
+	elements: {
+		title: string;
+		subMenu: SubmenuProps;
+	}[];
+}
 const Index: NextPage = () => {
+	const subMenus: SubmenuProps = { elements: [{ title: "Like, Follow and DM automatically , to make your account better placed in instagram Algorithm" }, { title: "Upload a picture from the database with its caption" }] };
+	const subMenus2: SubmenuProps = { elements: [{ title: "Download Pictures" }, { title: "Manage Database & Captions" }] };
+
+	const menu: MenuProps = {
+		elements: [
+			{
+				title: "Manage Account",
+				subMenu: subMenus,
+			},
+			{
+				title: "Manage Pictures",
+				subMenu: subMenus2,
+			},
+		],
+	};
+	const [active, setActive] = useState(menu.elements.map(() => false));
+	const clickHandler = (i: number) => {
+		setActive(
+			active.map((_value: any, index: number) => {
+				if (index === i) return true;
+				else return false;
+			})
+		);
+	};
+	console.log(active.findIndex((el) => el === true));
 	return (
-		<div className="bg-white w-screen min-h-screen flex-row gap-5 items-start justify-between">
-			<div className="">
-				<Menu />
+		<div className="bg-gray-50 w-full min-h-screen flex flex-row gap-10 items-start justify-start">
+			<div className="sticky top-0 left-0 w-fit m-0">
+				<Menu menu={menu} clickHandler={clickHandler} active={active} />
 			</div>
-			<div className=""></div>
+			{active.findIndex((el) => el === true) !== -1 && (
+				<div className="w-fit m-0">
+					<Submenu elements={menu.elements[active.findIndex((el) => el === true)].subMenu.elements} />
+				</div>
+			)}
 		</div>
 	);
 };
