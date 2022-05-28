2914ca1380d9a107c4d7550080e21e5cfb28b215
dalideco <dalissj1@gmail.com>
2022-05-28 15:47:13 +0100

    adding authentification middleware

--- backend/api/authentication.py
+++ backend/api/authentication.py
@@ -3,27 +3,27 @@ from rest_framework.decorators import api_view
 from authentification.models import User
 from .serializers import UserSerializer
 from datetime import datetime
+from django.utils.decorators import decorator_from_middleware
+from authentification.middleware import AuthMiddleware
 import bcrypt
 import jwt
 import os
 
 SECRET_KEY = os.environ.get('SECRET_KEY')
 
+
 @api_view(['GET'])
 def getData(request):
 
     token = request.headers["Auth-Token"]
 
-    try:
-        connection = jwt.decode(token, SECRET_KEY, algorithms=['HS256'])
-    except:
-        return Response("Invalid token please Reconnect", 300)
-
-    print(connection)
     users = User.objects.all()
     serializer = UserSerializer(users, many=True)
 
-    return Response(serializer.data)
+    return Response({
+        "data" : serializer.data,
+        "user": request.GET["user"]
+    })
 
 
 @api_view(['POST'])
@@ -34,9 +34,9 @@ def signup(request):
     users = User.objects.all()
     allSerializer = UserSerializer(users, many=True)
     for user in allSerializer.data:
-        if user.username == data["username"]:
+        if user["username"] == data["username"]:
             return Response("username already exists", status=400)
-        if (user.email == data["email"]):
+        if (user["email"] == data["email"]):
             return Response("email already in use", status=400)
 
     # transforming password
@@ -45,50 +45,51 @@ def signup(request):
     hash = bcrypt.hashpw(data["password"], salt)
     data["password"] = hash.decode('utf-8')
 
-    #saving new user
+    # saving new user
     newSerializer = UserSerializer(data=data)
-    if newSerializer.is_valid(): 
+    if newSerializer.is_valid():
         print("saving")
         newSerializer.save()
-    else: 
+    else:
         return Response(newSerializer._errors, 400)
 
     data.pop("password", None)
 
     return Response(data)
 
+
 @api_view(['POST'])
-def signin(request): 
+def signin(request):
     data = request.data
     users = User.objects.all()
     allSerializer = UserSerializer(users, many=True)
-    
+
     # checking if user exists
     found = None
     for user in allSerializer.data:
         if user["username"] == data["username"] or user["email"] == data["username"]:
             found = user
             break
-    if found == None :
+    if found == None:
         return Response("username/email not valid", 400)
-    
+
     # check if password is right
     encodedPass = data["password"].encode('utf-8')
     encodedHash = found["password"].encode('utf-8')
-    validPass = bcrypt.checkpw(encodedPass,encodedHash)
+    validPass = bcrypt.checkpw(encodedPass, encodedHash)
     if not validPass:
-        return Response("password is not valid", status=400) 
+        return Response("password is not valid", status=400)
 
     # sign jwt here
-    found.pop("password",None)
+    found.pop("password", None)
 
     toEncode = {
-        "data": found, 
+        "data": found,
         "logTime": datetime.now().strftime("%m/%d/%Y, %H:%M:%S")
     }
-    token = jwt.encode(toEncode,SECRET_KEY, algorithm="HS256")
+    token = jwt.encode(toEncode, SECRET_KEY, algorithm="HS256")
 
     return Response({
-        "message":"success", 
+        "message": "success",
         "token": token
     }, status=200)
---
+++ backend/authentification/middleware.py
@@ -0,0 +1,43 @@
+from django.http import HttpResponse
+import jwt
+import os
+import json
+
+SECRET_KEY = os.environ.get('SECRET_KEY')
+
+WITHOUT_AUTH = ['/auth/signin','/auth/signup']
+
+
+class AuthMiddleware:
+    def __init__(self, get_response):
+        self.get_response = get_response
+
+    def __call__(self, request):
+
+        path = request.path
+
+        foundRoutes = list(filter(lambda x: path.startswith(x), WITHOUT_AUTH))
+
+        if len(foundRoutes) != 0:
+            response = self.get_response(request)
+            return response
+
+        # check if valid
+        token = request.headers["Auth-Token"]
+        print('checking if valid')
+        try:
+            connection = jwt.decode(token, SECRET_KEY, algorithms=['HS256'])
+        except:
+            return HttpResponse("Invalid token please Reconnect", status=400)
+
+        # appending the request object with user
+        
+        my_request = request.GET.copy()
+        my_request["user"] = connection["data"]
+        request.GET = my_request
+
+        response = self.get_response(request)
+        return response
+
+    def process_view(self, request, view_func, view_args, view_kwargs):
+        return None
