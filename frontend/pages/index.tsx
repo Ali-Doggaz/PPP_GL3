813326e47e17d71be978e00e116e34ff7184680d
Mohamed Amine Hamdouni <mohamed@mvst.co>
2022-05-28 12:00:49 +0100

    adds toast and stats

--- frontend/pages/index.tsx
+++ frontend/pages/index.tsx
@@ -1,9 +1,12 @@
 import { Button, Checkbox, Input } from "@nextui-org/react";
 import { NextPage } from "next";
 import { useState } from "react";
+import { PieChart, Pie, Legend, RadialBar, RadialBarChart, Tooltip, PolarAngleAxis, PolarGrid, PolarRadiusAxis, Radar, RadarChart } from "recharts";
 import { FormDownload, FormFake, FormUpload } from "../components/Forms";
 import Menu from "../components/Menu";
+import { Stats } from "../components/Stats";
 import { Submenu } from "../components/Submenu";
+import { Toast } from "../components/Toast";
 export interface SubmenuProps {
 	elements: { title: string }[];
 }
@@ -48,15 +51,23 @@ const Index: NextPage = () => {
 			})
 		);
 	};
+	const [show, setShow] = useState(false);
 	return (
 		<div className="bg-gray-50 w-full min-h-screen flex flex-col md:flex-row gap-10 items-center md:items-start justify-center md:justify-start">
+			<Toast
+				setShow={setShow}
+				show={show}
+			/>
 			<div className="md:sticky top-0 left-0 w-fit m-0">
 				<Menu menu={menu} clickHandler={clickHandler} active={active} />
 			</div>
 			{active.findIndex((el) => el === true) !== -1 && (
 				<div className="flex flex-col gap-10 grow justify-start items-start">
-					<div className="w-fit m-0">
+					<div className="w-fit m-0 flex md:flex-row flex-col-reverse flex-stretch gap-10">
 						<Submenu submenu={menu.elements[active.findIndex((el) => el === true)].subMenu} clickHandler={clickHandlerSub} active={activeSub} />
+						<div className="flex h-full justify-center items-center">
+							<Stats />
+						</div>
 					</div>
 					{active[0] && activeSub[0] && (
 						<div className="bg-white rounded-3xl shadow-lg p-10 w-full max-w-[1000px] md:ml-3">
