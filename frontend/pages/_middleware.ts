7ddf4664148133d5507d2cf1679a4e5fd256f856
Mohamed Amine Hamdouni <mohamed@mvst.co>
2022-05-28 23:07:48 +0100

    adds JWT authentification

--- frontend/pages/_middleware.ts
+++ frontend/pages/_middleware.ts
@@ -1,11 +1,18 @@
+import { verify } from "jsonwebtoken";
 import { NextRequest, NextResponse } from "next/server";
-
+const secret = process.env.SECRET_KEY;
 export default function middleware(req: NextRequest) {
 	const { cookies } = req;
 	const jwt = cookies.JWT;
 	const { pathname, origin } = req.nextUrl;
 	if (pathname === undefined || pathname === "/") {
 		if (jwt === undefined) return NextResponse.redirect(origin + "/signin");
+		try {
+			verify(jwt, secret!);
+			return NextResponse.next();
+		} catch (e) {
+			return NextResponse.redirect(origin + "/signin");
+		}
 	}
 
 	return NextResponse.next();
